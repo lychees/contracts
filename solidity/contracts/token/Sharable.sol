cad1f86be663f51fe064bf2f022073e1ca8d7daa
Frank Wei <frank@frankwei.xyz>
2018-07-09 19:16:05 +0800

    fix indentation

--- solidity/contracts/token/Sharable.sol
+++ solidity/contracts/token/Sharable.sol
@@ -10,39 +10,39 @@ import "./SmartToken.sol";
 
 library SafeMath {
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    /**
+    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
@@ -117,14 +117,15 @@ contract Sharable is SmartToken {
         return true;
     }
  
-    function transferFrom(address _from, address _to, uint256 _value) public transfersAllowed returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) 
+    public transfersAllowed returns (bool success) {
         updateTokenAge(msg.sender);
         updateTokenAge(_to);
         assert(super.transferFrom(_from, _to, _value));
         return true;
-	}
+    }
 
-    function maintainSharedPool() payable  public {
+    function maintainSharedPool() payable    public {
         sharedPool = sharedPool.add(msg.value);
     }
 }
